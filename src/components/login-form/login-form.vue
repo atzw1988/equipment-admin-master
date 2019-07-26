aa32908c7a5a8857ce5b108da00f042292cd8423
ICON <386553646@qq.com>
2019-07-26 18:29:26 +0800

    增加验证码

--- src/components/login-form/login-form.vue
+++ src/components/login-form/login-form.vue
@@ -14,49 +14,65 @@
         </span>
       </Input>
     </FormItem>
+    <FormItem prop="code">
+      <Row>
+        <Col span="12" style="margin-top:4px">
+          <Input v-model="form.code" placeholder="请输入验证码"></Input>
+        </Col>
+        <Col span="12">
+          <div class="login-code" @click="refreshCode">
+            <s-identify :identifyCode="identifyCode"></s-identify>
+          </div>
+        </Col>
+      </Row>
+    </FormItem>
     <FormItem>
       <Button @click="handleSubmit" type="primary" long>登录</Button>
     </FormItem>
   </Form>
 </template>
 <script>
+import SIdentify from './verification-code'
 export default {
   name: 'LoginForm',
-  props: {
-    userNameRules: {
-      type: Array,
-      default: () => {
-        return [
-          { required: true, message: '账号不能为空', trigger: 'blur' }
-        ]
-      }
-    },
-    passwordRules: {
-      type: Array,
-      default: () => {
-        return [
-          { required: true, message: '密码不能为空', trigger: 'blur' }
-        ]
+  components: { SIdentify },
+  data () {
+    const validateCode = (rule, value, callback) => {
+      if (this.identifyCode !== value.toUpperCase()) {
+        this.form.code = ''
+        this.refreshCode()
+        callback(new Error('请输入正确的验证码'))
+      } else {
+        callback()
       }
     }
-  },
-  data () {
     return {
+      rules: {
+        userName: [
+          { required: true, message: '账号不能为空', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空', trigger: 'blur' },
+          { validator: validateCode, trigger: 'blur' }
+        ]
+      },
       form: {
         userName: 'super_admin',
-        password: ''
-      }
+        password: '',
+        code: ''
+      },
+      identifyCodes: '1234567890ABCDEFGHJKLMNOPQRSTUVWXYZ',
+      identifyCode: ''
     }
   },
   computed: {
-    rules () {
-      return {
-        userName: this.userNameRules,
-        password: this.passwordRules
-      }
-    }
+
   },
   methods: {
+    // 提交账号密码
     handleSubmit () {
       this.$refs.loginForm.validate((valid) => {
         if (valid) {
@@ -66,7 +82,25 @@ export default {
           })
         }
       })
+    },
+    // 更换验证码
+    refreshCode () {
+      this.identifyCode = ''
+      this.makeCode(this.identifyCodes, 4)
+      console.log(this.identifyCode)
+    },
+    // 生成随即
+    randomNum (min, max) {
+      return Math.floor(Math.random() * (max - min) + min)
+    },
+    makeCode (o, l) {
+      for (let i = 0; i < l; i++) {
+        this.identifyCode += this.identifyCodes[this.randomNum(0, this.identifyCodes.length)]
+      }
     }
+  },
+  mounted () {
+    this.refreshCode()
   }
 }
 </script>
