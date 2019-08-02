6b489021c6f0d76370f12bcf9f2b39b242f08f67
ICON <386553646@qq.com>
2019-08-02 16:05:45 +0800

    记住密码

--- src/libs/util.js
+++ src/libs/util.js
@@ -26,6 +26,35 @@ const showThisMenuEle = (item, access) => {
     else return false
   } else return true
 }
+
+export const setCookie = (c_name, value, expiremMinutes) => {
+  var exdate = new Date()
+  exdate.setTime(exdate.getTime() + expiremMinutes * 60 * 1000)
+  document.cookie = c_name + '=' + escape(value) + ((expiremMinutes == null) ? '' : ';expires=' + exdate.toGMTString())
+}
+
+export const getCookie = (c_name) => {
+  if (document.cookie.length > 0) {
+    let c_start = document.cookie.indexOf(c_name + '=')
+    if (c_start !== -1) {
+      c_start = c_start + c_name.length + 1
+      let c_end = document.cookie.indexOf(';', c_start)
+      if (c_end !== -1) {
+        return unescape(document.cookie.substring(c_start, c_end))
+      }
+    }
+  }
+  return ''
+}
+
+export const delCookie = (c_name) => {
+  let exp = new Date()
+  exp.setTime(exp.getTime() - 1)
+  let cval = this.getCookie(c_name)
+  if (cval !== null) {
+    document.cookie = c_name + '=' + cval + ';expires=' + exp.toGMTString()
+  }
+}
 /**
  * @param {Array} list 通过路由列表得到菜单列表
  * @returns {Array}
